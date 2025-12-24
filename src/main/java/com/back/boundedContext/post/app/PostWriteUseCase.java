5de539a7b2f26c2b124ec15a62506d6891ad660c
pty10510 <pty10510@gmail.com>
2025-12-24 10:38:03 +0900

    0011

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
+import com.back.global.RsData.RsData;
 import com.back.global.eventPublisher.EventPublisher;
 import com.back.shared.post.dto.PostDto;
 import com.back.shared.post.event.PostCreatedEvent;
@@ -18,7 +19,7 @@ public class PostWriteUseCase {
 	private final PostRepository postRepository;
 	private final EventPublisher eventPublisher;
 
-	public Post write(Member author, String title, String content) {
+	public RsData<Post> write(Member author, String title, String content) {
 		Post post = postRepository.save(new Post(author, title, content));
 
 		eventPublisher.publish(
@@ -27,6 +28,6 @@ public class PostWriteUseCase {
 			)
 		);
 
-		return post;
+		return new RsData<>("201-1", "%d 번 글이 생성되었습니다.".formatted(post.getId()), post);
 	}
 }
